e71492a8e111f0d59675ccb471d6ab6d443589af
Alicia Santiago <albrown07@mac.com>
2019-02-13 23:40:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 def prime?(num)
   if num >= 2
   (2..(num - 1)).each do |i|
-  return false if num % i == 0 || num < 0 
+  num % i != 0
   end
   true
 end
 
 #(2..num/2).none?{|i| num % i == i}
+#return false if num % i == 0 || num < 0 
