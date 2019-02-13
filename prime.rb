d7b808a94d109468761f196437b60637698cbd5c
Alicia Santiago <albrown07@mac.com>
2019-02-13 23:43:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,13 +10,3 @@ end
 
 #(2..num/2).none?{|i| num % i == i}
 #return false if num % i == 0 || num < 0 
-
-#def prime?(number)
- # if number >= 2
-   # (2..number - 1).all? do |x|
-   #   number % x != 0
-   # end
- # else
-  #  return false
- # end
-#end
