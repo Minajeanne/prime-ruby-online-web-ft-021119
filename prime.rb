1390a97e521a79fe777028200dd2b6ef0e0ed127
Alicia Santiago <albrown07@mac.com>
2019-02-13 23:25:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   i = 1
-  (2..n/2).none?{|i| num % i == 0}
-  return false if num % i == 0
+  (2..num/2).none?{|i| num % i == 0}
+  return false if num % i == 0 || num < 0 
   else
   return true
 
